fc528aff77a50f8c8877e970082cfee33bef8712
AnwarHb <74914639+AnwarHb@users.noreply.github.com>
2021-01-09 13:45:25 +0200

    Update taskThread.hpp

--- taskThread.hpp
+++ taskThread.hpp
@@ -1,23 +1,28 @@
 #ifndef _TASKTHREAD_H
 #define _TASKTHREAD_H
 
+#include "../Part1/PCQueue.hpp"
+#include "task.hpp"
 #include "Thread.hpp"
 #include "utils.hpp"
 
 // inherit from thread // 
 class taskThread:public Thread{
 private:
-	int linesNum;
-	int pointTostart;
+	//int linesNum;//
+	//int pointTostart;// will not work
+	pthread_mutex_t *m; // mutex//
+	vector<double>* histogram_of_tile;
+	PCQueue<Task>* tasksQueue; // added 			
 	vector<vector<int>>* curr_table;
 	vector<vector<int>>* nxt_table;
-	vector<float>* histogram_of_tile;
-	pthread_mutex_t *m; // mutex//
-	bool phase;
-	float difTime;
+	// instead of phase and diff time changed to  unsigned for cheking finish phase//
+	uint* phase_phinished;
+
 public:
-	taskThread(uint idThread,int numLines,int start,vector<vector<int>>* current,vector<vector<int>>* next,
-	           vector<float>* hist, pthread_mutex_t* mutex);
+	taskThread(uint idThread,pthread_mutex_t* mutex,vector<double>* hist,
+	PCQueue<Task>* tasks,vector<vector<int>>* current,vector<vector<int>>* next,
+	uint* stopPhase);          
 	~taskThread()=default;
 	void thread_workload() override;
 	};
